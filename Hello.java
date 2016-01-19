72668833e39290c313774fb7f5cfffabbb0719ff
heerahwang <com2132_@naver.com>
2016-01-19 15:12:55 +0900

    Hello.java modified on GitHub.
    optional

--- Hello.java
+++ Hello.java
@@ -1,11 +1,11 @@
+//For command line git tutorial
+//GitHub modification
+
 public class Hello{
-	public static void main(String[] args)
-{
+	public static void main(String[] args){
 	System.out.println("Hello git");
 	System.out.println("what is git?");
-	System.out.println("what!!!!!!");
 	System.out.println("What's DVCS?");
 	System.out.printiln("Distributed Version Control system");
->>>>>>> sist
-}
+	}
 }
